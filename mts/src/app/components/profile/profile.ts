77fdfb415468195d840160f482d78e6cce149e63
Karthikeyan S <karthikeyanswami2005@gmail.com>
2026-02-15 13:13:49 +0000

    Profile component changed

--- mts/src/app/components/profile/profile.ts
+++ mts/src/app/components/profile/profile.ts
@@ -3,6 +3,7 @@ import { AccountStatus } from '../../core/enums/account-status.enum';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AccountInterface } from '../../core/interface/accountInterface';
 import { Account } from '../../core/services/account';
+import { AuthService } from '../../core/services/auth';
 
 @Component({
   selector: 'app-profile',
@@ -11,6 +12,7 @@ import { Account } from '../../core/services/account';
   styleUrl: './profile.css',
 })
 export class Profile {
+  isLoggedin: boolean = false;
  userName  : number= 0;
  account: AccountInterface=
                 {
@@ -23,10 +25,13 @@ export class Profile {
                 };
  
   
-    constructor(private route: ActivatedRoute,private router: Router,private accountService: Account, private cd:ChangeDetectorRef) {}
+    constructor(private route: ActivatedRoute,private router: Router,private accountService: Account, private cd:ChangeDetectorRef,private authService: AuthService) {}
   
   ngOnInit() : void{
-    
+    this.isLoggedin = this.authService.isUserLoggedin();
+    if (!this.isLoggedin){
+      this.router.navigateByUrl('login');
+    }
     this.userName = Number(this.route.snapshot.paramMap.get('username'));
     this.accountService.getAccount(this.userName).subscribe({
       next: data => {this.account = data; console.log(this.account);this.cd.detectChanges();},
